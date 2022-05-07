0865c28cf533afb5e375b92b106ca6bbbd2a7abf
vladislavabrosimov <abrosimov94vladislav@gmail.com>
2022-05-07 22:03:58 +0200

    ME-3/Eureka: eureka added

--- customer/src/main/java/org/example/CustomerService.java
+++ customer/src/main/java/org/example/CustomerService.java
@@ -19,7 +19,7 @@ public class CustomerService {
                 .build();
         customerRepository.saveAndFlush(customer);
         FraudCheckResponse fraudResponse = restTemplate.getForObject(
-                "http://localhost:8081/api/v1/fraud-check/{customerId}",
+                "http://FRAUD/api/v1/fraud-check/{customerId}",
                 FraudCheckResponse.class,
                 customer.getId()
         );
